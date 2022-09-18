61c4684e6aa9f4a4012b69a16ca58ee4991e8c79
Alex Cattell <abcattell91@gmail.com>
2022-09-18 16:15:18 +0100

    Added Color Context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext'
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
   renderSubmit(value) {
@@ -9,14 +10,21 @@ class Button extends React.Component {
   // this is how to hook up a context object to a class component
   // static adds a property to the class itself
   // Button.contextType = LanguageContext
+  renderButton(color) {
+    return (
+    <button className={`ui button ${color}`}>
+      <LanguageContext.Consumer>
+        {(value) => this.renderSubmit(value) }
+      </LanguageContext.Consumer>
+    </button>
+    )
+  }
+
   render () {
     return (
-      <button className="ui button primary">
-      {/* consumer always gets one child/consumer */}
-        <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value) }
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+        {color => this.renderButton(color)}
+      </ColorContext.Consumer>
     )
   };
 };
